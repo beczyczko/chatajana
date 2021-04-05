9fc1ab3a534817d5bbf412887799ef37edc38e19
beczyczko <dbeczyczko@gmail.com>
2021-04-05 13:48:03 +0200

    add gallery section

--- cj/pages/index.js
+++ cj/pages/index.js
@@ -2,15 +2,23 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 const Home = ({ content, error }) => {
+  const galeryStartImageNo = 4;
+
   if (error) {
     return <div>An error occured: {error.message}</div>;
   }
 
-  const images = content.images;
+  const images = content.images.map((img, i) => ({i, img}));
   console.log(images);
 
   const imageSource = (imageNo, size) => {
-    return `http://localhost:1337${images[imageNo].formats[size].url}`;
+    const imageInDesiredFormat = images[imageNo].img.formats[size];
+    const imageRelativeUrl = imageInDesiredFormat ? imageInDesiredFormat.url : images[imageNo].img.url;
+    return `http://localhost:1337${imageRelativeUrl}`;
+  }
+
+  const galleryImages = () => {
+    return images.slice(galeryStartImageNo, images.length);
   }
 
   return (
@@ -70,77 +78,104 @@ const Home = ({ content, error }) => {
 
         <div className="container-fluid content">
           <div className="container">
-              <div className="row">
-                  <aside className="col-sm-7">
-                      <a className="content-md example-image-link" href={imageSource(0, 'large')} data-lightbox="gallery">
-                          <img className="img-responsive" src={imageSource(0, 'medium')} alt="" />
-                      </a>
-                      <img src={imageSource(0, 'small')} className="content-sm img-responsive" />
-                  </aside>
-                  <section className="col-sm-5">
-                      <div className="txt description">Chata Jana zaprasza do Łeby. Polecamy ciepły klimat naszego pensjonatu. Wybierając naszą ofertę spośród innych <a href="http://meteor-turystyka.pl/noclegi,morze,57.html" target='blank'>opcji noclegów nad morzem</a>, z pewnością nie pożałujesz! Dysponujemy pokojami 2-5 osobowymi z łazienkami, balkonami i lodówkami. Do dyspozycji gości jest też w pełni wyposażony aneks kuchenny. Teren posesji jest ogrodzony, parking bezpłatny.</div>
-                  </section>
-              </div>
-              <div className="row">
-                  <aside className="col-sm-7 col-sm-push-5">
-                      <a className="content-md example-image-link" href={imageSource(1, 'large')} data-lightbox="gallery">
-                          <img className="img-responsive" src={imageSource(1, 'medium')} alt="" />
-                      </a>
-                      <img src={imageSource(1, 'small')} className="content-sm img-responsive" />
-                  </aside>
-                  <section className="col-sm-5 col-sm-pull-7">
-                      <div className="txt">
-                          <h3>Udostępniamy także:</h3>
-                          <ul>
-                              <li>miejsce na samochód</li>
-                              <li>grill ogrodowy</li>
-                              <li>plac zabaw dla dzieci</li>
-                              <li>rowery (płatne)</li>
-                              <li>parawany</li>
-                          </ul>
-                      </div>
-                  </section>
-              </div>
-              <div className="row">
-                  <aside className="col-sm-7">
-                      <a className="content-md example-image-link" href={imageSource(2, 'large')} data-lightbox="gallery">
-                          <img className="img-responsive" src={imageSource(2, 'medium')} alt="" />
-                      </a>
-                      <img src={imageSource(2, 'small')} className="content-sm img-responsive" />
-                  </aside>
-                  <section className="col-sm-5">
-                      <div className="txt">
-                          <ul>
-                              <li>Odległość do plaży - 1,5 km</li>
-                              <li>Park dinozaurów - 300 m</li>
-                              <li>Stadniny koni, kajaki - 1,2 km</li>
-                              <li>Sea Park w Sarbsku - 8 km</li>
-                              <li>Sklepy, restauracje - 300 m</li>
-                          </ul>
-                      </div>
-                  </section>
-              </div>
-              <div className="row">
-                  <aside className="col-sm-7 col-sm-push-5">
-                      <a className="content-md example-image-link" href={imageSource(3, 'large')} data-lightbox="gallery">
-                          <img className="img-responsive" src={imageSource(3, 'medium')} alt="" />
-                      </a>
-                      <img src={imageSource(3, 'small')} className="content-sm img-responsive" />
-                  </aside>
-                  <section className="col-sm-5 col-sm-pull-7">
-                      <div className="txt">
-                          <ul>
-                              <li>Atrakcyjne ceny</li>
-                              <li>Spokojna okolica</li>
-                              <li>Komfortowe warunki</li>
-                              <li>Cena do uzgodnienia</li>
-                              <li>Zapraszamy do nas!</li>
-                          </ul>
-                      </div>
-                  </section>
-              </div>
+            <div className="row">
+                <aside className="col-sm-7">
+                    <a className="content-md example-image-link" href={imageSource(0, 'large')} data-lightbox="gallery">
+                        <img className="img-responsive" src={imageSource(0, 'medium')} alt="" />
+                    </a>
+                    <img src={imageSource(0, 'small')} className="content-sm img-responsive" />
+                </aside>
+                <section className="col-sm-5">
+                    <div className="txt description">Chata Jana zaprasza do Łeby. Polecamy ciepły klimat naszego pensjonatu. Wybierając naszą ofertę spośród innych <a href="http://meteor-turystyka.pl/noclegi,morze,57.html" target='blank'>opcji noclegów nad morzem</a>, z pewnością nie pożałujesz! Dysponujemy pokojami 2-5 osobowymi z łazienkami, balkonami i lodówkami. Do dyspozycji gości jest też w pełni wyposażony aneks kuchenny. Teren posesji jest ogrodzony, parking bezpłatny.</div>
+                </section>
+            </div>
+            <div className="row">
+                <aside className="col-sm-7 col-sm-push-5">
+                    <a className="content-md example-image-link" href={imageSource(1, 'large')} data-lightbox="gallery">
+                        <img className="img-responsive" src={imageSource(1, 'medium')} alt="" />
+                    </a>
+                    <img src={imageSource(1, 'small')} className="content-sm img-responsive" />
+                </aside>
+                <section className="col-sm-5 col-sm-pull-7">
+                    <div className="txt">
+                        <h3>Udostępniamy także:</h3>
+                        <ul>
+                            <li>miejsce na samochód</li>
+                            <li>grill ogrodowy</li>
+                            <li>plac zabaw dla dzieci</li>
+                            <li>rowery (płatne)</li>
+                            <li>parawany</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+            <div className="row">
+                <aside className="col-sm-7">
+                    <a className="content-md example-image-link" href={imageSource(2, 'large')} data-lightbox="gallery">
+                        <img className="img-responsive" src={imageSource(2, 'medium')} alt="" />
+                    </a>
+                    <img src={imageSource(2, 'small')} className="content-sm img-responsive" />
+                </aside>
+                <section className="col-sm-5">
+                    <div className="txt">
+                        <ul>
+                            <li>Odległość do plaży - 1,5 km</li>
+                            <li>Park dinozaurów - 300 m</li>
+                            <li>Stadniny koni, kajaki - 1,2 km</li>
+                            <li>Sea Park w Sarbsku - 8 km</li>
+                            <li>Sklepy, restauracje - 300 m</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
+            <div className="row">
+                <aside className="col-sm-7 col-sm-push-5">
+                    <a className="content-md example-image-link" href={imageSource(3, 'large')} data-lightbox="gallery">
+                        <img className="img-responsive" src={imageSource(3, 'medium')} alt="" />
+                    </a>
+                    <img src={imageSource(3, 'small')} className="content-sm img-responsive" />
+                </aside>
+                <section className="col-sm-5 col-sm-pull-7">
+                    <div className="txt">
+                        <ul>
+                            <li>Atrakcyjne ceny</li>
+                            <li>Spokojna okolica</li>
+                            <li>Komfortowe warunki</li>
+                            <li>Cena do uzgodnienia</li>
+                            <li>Zapraszamy do nas!</li>
+                        </ul>
+                    </div>
+                </section>
+            </div>
           </div>
-      </div>
+        </div>
+
+        <div id="gallery-section" className="container-fluid content">
+          <div className="container">
+            <div className="row">
+              <h1>Galeria</h1>
+            </div>
+            <div className="row">
+              <div className="content-md">
+                {galleryImages().map(({i, img}) => (
+                  <a key={`gallery-img-${i}`}
+                     className="example-image-link"
+                     href={imageSource(i, 'large')}
+                     data-lightbox="gallery">
+                       <img className="image-thumb" src={imageSource(i, 'thumbnail')} alt="" />
+                  </a>
+                ))}
+              </div>
+              <div className="content-sm">
+                {galleryImages().map(({i, img}) => (
+                  <img key={`gallery-img-${i}`}
+                       src={imageSource(i, 'small')}
+                       className="img-responsive" />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
 
       {/* <footer className={styles.footer}> */}
