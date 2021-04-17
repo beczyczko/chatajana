02a7c7778596e1d2cae6bd22cceed0b0be0207cf
beczyczko <dbeczyczko@gmail.com>
2021-04-17 18:55:32 +0200

    introduce contact page

--- cj/pages/index.js
+++ cj/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import Gallery from '../components/Gallery';
+import Contact from '../components/Contact';
 import { imageSource } from '../common/imageHelper';
 import { cmsBaseUrl } from '../config';
 
@@ -184,22 +184,7 @@ const Home = ({ content, error }) => {
                     </div>
                 </div>
 
-                <div id="contact" className="container-fluid content">
-                    <div className="container" id="contact">
-                        <h1>Kontakt</h1>
-                        <div className="content-sm">
-                            <a href="tel:694572228"><h2>694 572 228</h2></a>
-                            <a href="tel:598661174"><h2>59 866 11 74</h2></a>
-                            <a href="mailto:chatajanaleba@gmail.com?Subject=Zapytanie%20z%20chatajana.pl">
-                                <h2>chatajanaleba@gmail.com</h2></a>
-                        </div>
-                        <div className="content-md">
-                            <h2>694 572 228</h2>
-                            <h2>59 866 11 74</h2>
-                            <h2>chatajanaleba@gmail.com</h2>
-                        </div>
-                    </div>
-                </div>
+                <Contact/>
 
                 <div className="container-fluid content">
                     <div className="container">
