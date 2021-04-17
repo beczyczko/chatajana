052409cb23d514f456613b1c57ffb0315b4030f7
beczyczko <dbeczyczko@gmail.com>
2021-04-17 20:27:18 +0200

    partners component created

---
+++ cj/components/Partners.js
@@ -0,0 +1,38 @@
+const Partners = () => {
+    return (
+        <div className="container-fluid content">
+            <div className="container container-partners">
+                <a href='http://www.nocowanie.pl/noclegi/leba/kwatery_i_pokoje/2084/' target='_blank'
+                   title='Chata Jana'>
+                    <img src='http://img.nocowanie.pl/files/photos/exban/1_1.gif' border='0'
+                         alt='Chata Jana' width='108' height='70'/>
+                </a>
+                <a href='http://www.nocowanie.pl/noclegi/leba/' target='_blank' title='Łeba'><img
+                    src='http://img.nocowanie.pl/files/photos/exban/1_2.gif' border='0' alt='Łeba'
+                    width='21' height='70'/></a>
+                <a href='http://www.nocowanie.pl/' target='_blank' title='noclegi'>
+                    <img src='http://img.nocowanie.pl/files/photos/exban/1_3.gif' border='0' alt='noclegi'
+                         width='31' height='70'/>
+                </a>
+                <a href="http://www.eholiday.pl/noclegi-nn3873.html"
+                   title="Pokoje Gościnne Chata Jana Łeba"
+                   target="_blank">
+                    <img src="http://www.eholiday.pl/img/partner/l1a.gif"
+                         alt="Pokoje Gościnne Chata Jana Łeba" border="0"
+                         width="52" height="26"/>
+                </a>
+                <a href="http://www.eholiday.pl/noclegi-leba-53017.html" title="noclegi Łeba"
+                   target="_blank">
+                    <img src="http://www.eholiday.pl/img/partner/l1b.gif" alt="noclegi Łeba" border="0"
+                         width="22" height="26"/>
+                </a>
+                <a href="http://www.eholiday.pl" title="noclegi" target="_blank">
+                    <img src="http://www.eholiday.pl/img/partner/l1c.gif" alt="noclegi" border="0"
+                         width="22" height="26"/>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Partners;
--- cj/pages/index.js
+++ cj/pages/index.js
@@ -5,6 +5,7 @@ import Gallery from '../components/Gallery';
 import Contact from '../components/Contact';
 import AdditionalInfo from '../components/AdditionalInfo';
 import Bon from '../components/Bon';
+import Partners from '../components/Partners';
 
 const Home = ({ content, error }) => {
     const galleryStartImageNo = 4;
@@ -192,38 +193,7 @@ const Home = ({ content, error }) => {
 
                 <Bon/>
 
-                <div className="container-fluid content">
-                    <div className="container container-partners">
-                        <a href='http://www.nocowanie.pl/noclegi/leba/kwatery_i_pokoje/2084/' target='_blank'
-                           title='Chata Jana'>
-                            <img src='http://img.nocowanie.pl/files/photos/exban/1_1.gif' border='0'
-                                 alt='Chata Jana' width='108' height='70'/>
-                        </a>
-                        <a href='http://www.nocowanie.pl/noclegi/leba/' target='_blank' title='Łeba'><img
-                            src='http://img.nocowanie.pl/files/photos/exban/1_2.gif' border='0' alt='Łeba'
-                            width='21' height='70'/></a>
-                        <a href='http://www.nocowanie.pl/' target='_blank' title='noclegi'>
-                            <img src='http://img.nocowanie.pl/files/photos/exban/1_3.gif' border='0' alt='noclegi'
-                                 width='31' height='70'/>
-                        </a>
-                        <a href="http://www.eholiday.pl/noclegi-nn3873.html"
-                           title="Pokoje Gościnne Chata Jana Łeba"
-                           target="_blank">
-                            <img src="http://www.eholiday.pl/img/partner/l1a.gif"
-                                 alt="Pokoje Gościnne Chata Jana Łeba" border="0"
-                                 width="52" height="26"/>
-                        </a>
-                        <a href="http://www.eholiday.pl/noclegi-leba-53017.html" title="noclegi Łeba"
-                           target="_blank">
-                            <img src="http://www.eholiday.pl/img/partner/l1b.gif" alt="noclegi Łeba" border="0"
-                                 width="22" height="26"/>
-                        </a>
-                        <a href="http://www.eholiday.pl" title="noclegi" target="_blank">
-                            <img src="http://www.eholiday.pl/img/partner/l1c.gif" alt="noclegi" border="0"
-                                 width="22" height="26"/>
-                        </a>
-                    </div>
-                </div>
+                <Partners/>
 
             </main>
 
